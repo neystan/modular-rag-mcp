647ec96f14ba098eae9cffd595cdf1d1114f412b
neystan <2918360904@qq.com>
2026-05-05 13:36:56 +0800

    update c7

--- src/ingestion/transform/__init__.py
+++ src/ingestion/transform/__init__.py
@@ -2,10 +2,12 @@
 
 from ingestion.transform.base_transform import BaseTransform
 from ingestion.transform.chunk_refiner import ChunkRefiner
+from ingestion.transform.image_captioner import ImageCaptioner
 from ingestion.transform.metadata_enricher import MetadataEnricher
 
 __all__ = [
     "BaseTransform",
     "ChunkRefiner",
+    "ImageCaptioner",
     "MetadataEnricher",
 ]
